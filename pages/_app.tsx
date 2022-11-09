a16a6992645c944f3c640e5562aa21642b1667d5
puddingg <yuki.pdg@gmail.com>
2022-11-09 18:43:43 +0900

    changed style for codeblock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "../node_modules/next/app";
 import "../styles/global.css";
-import "prismjs/themes/prism.css";
+import "prismjs/themes/prism-tomorrow.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
